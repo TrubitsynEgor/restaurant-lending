034dd1ec18af23b28f466494a0a8b7ae0d85a78c
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-02-21 23:53:46 +0500

    all rdy

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -5,9 +5,21 @@ Swiper.use([Navigation, Pagination]);
 const sliderMenu = document.querySelector('.menu__slider');
 if (sliderMenu) {
   const menuSlider = new Swiper(sliderMenu, {
-    slidesPerView: 3,
     loop: true,
+    slidesPerView: 3,
     spaceBetween: 30,
+    breakpoints: {
+      310: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    }
   });
 }
 
--- src/js/main.js
+++ src/js/main.js
@@ -4,5 +4,20 @@ import './_functions';
 import './_components';
 
 
+'use-strict'
+
+const smoothLinks = document.querySelectorAll('.menu-link');
+for (let smoothLink of smoothLinks) {
+  smoothLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = smoothLink.getAttribute('href');
+
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+};
+
 
 
